dc32a9653bae49a77528bb5b232501b7a1c52c1d
Florian <florian.a@live.fr>
2019-06-04 12:45:23 +0200

    edited configuration

--- scripts/update_configuration.sh
+++ scripts/update_configuration.sh
@@ -7,3 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 sudo -u postgres psql -f $DIR/../sql/update_configuration.sql
 
 service postgresql restart
+
+sudo -u postgres psql -c "SELECT name, setting FROM pg_settings WHERE name LIKE 'port' OR name LIKE 'work_mem';"
--- sql/update_configuration.sql
+++ sql/update_configuration.sql
@@ -1,3 +1,2 @@
 ALTER SYSTEM SET PORT = "1234";
 ALTER SYSTEM SET work_mem = '50MB';
-SELECT name, setting FROM pg_settings WHERE name LIKE 'port' OR name LIKE 'work_mem';
