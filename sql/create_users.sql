b0259d57820889506638e00fd3627ca92d679d63
Florian <florian.a@live.fr>
2019-06-05 12:55:45 +0200

    Updated create users script

--- sql/create_users.sql
+++ sql/create_users.sql
@@ -1,5 +1,9 @@
-DROP DATABASE super_admin;
-DROP ROLE super_admin;
+DROP DATABASE IF EXISTS super_admin;
+DROP ROLE IF EXISTS super_admin;
+DROP ROLE IF EXISTS github_events;
 
 CREATE ROLE super_admin LOGIN PASSWORD '12345' SUPERUSER;
 CREATE DATABASE super_admin OWNER super_admin;
+
+CREATE ROLE github_events LOGIN PASSWORD '12345';
+ALTER DATABASE github_events OWNER TO github_events
