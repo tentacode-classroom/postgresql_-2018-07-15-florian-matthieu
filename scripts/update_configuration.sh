ee865999e8c38126288aea966d80ddfc3bd746a6
Florian <florian.a@live.fr>
2019-06-05 12:27:01 +0200

    Changed update_configuration script

--- scripts/update_configuration.sh
+++ scripts/update_configuration.sh
@@ -9,3 +9,6 @@ sudo -u postgres psql -f $DIR/../sql/update_configuration.sql
 service postgresql restart
 
 sudo -u postgres psql -c "SELECT name, setting FROM pg_settings WHERE name IN ('port', 'work_mem');"
+
+sudo -u postgres psql -c 'ALTER SYSTEM RESET all'
+sudo service postgresql restart
