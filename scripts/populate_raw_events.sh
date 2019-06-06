32ddfcd66397ff3e3e466c746dbaac01a75706b4
Florian <florian.a@live.fr>
2019-06-06 18:16:23 +0200

    Fixed permissions and owner of table

--- scripts/populate_raw_events.sh
+++ scripts/populate_raw_events.sh
@@ -5,11 +5,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 #Executer script SQL
-sudo -u postgres psql -f $DIR/../sql/populate_raw_events.sql
+psql --username=github_events -f $DIR/../sql/populate_raw_events.sql
 
 #insertion des données dans la BDD
-php populate_raw_events.php
+php $DIR/populate_raw_events.php
 
 
 #connexion a la BDD puis affichage des 10 premières lignes
-
+psql --username=github_events -c "SELECT * FROM events_raw LIMIT 10"
