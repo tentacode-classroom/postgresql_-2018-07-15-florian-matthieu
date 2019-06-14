0b018ee53e928b52db8eb674d235847fad13f8d1
Florian <florian.a@live.fr>
2019-06-14 13:39:52 +0200

    Updated stats

--- sql/create_stats.sql
+++ sql/create_stats.sql
@@ -1,5 +1,5 @@
 DROP VIEW most_commits;
 
-CREATE VIEW most_commits AS SELECT push_events.id, name, created_at, branch, commit_count FROM push_events INNER JOIN repo ON push_events.id_repo = repo.id ORDER BY commit_count DESC LIMIT 15;
+CREATE VIEW most_commits AS SELECT push_events.id, name as repo_name, login AS user, created_at, branch, commit_count FROM push_events INNER JOIN repo ON push_events.id_repo = repo.id INNER JOIN actor ON push_events.id_actor = actor.id ORDER BY commit_count DESC LIMIT 15;
 
 SELECT * FROM most_commits;
